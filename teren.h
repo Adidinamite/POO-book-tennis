f00c9bc5b44b434a3df87628e8172fded1deb06d
Adidinamite <61920198+Adidinamite@users.noreply.github.com>
2024-03-28 16:54:44 +0200

    Update teren.h

--- teren.h
+++ teren.h
@@ -5,66 +5,91 @@
 #include <string>
 #include <vector>
 #include "templates.h"
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
 class Teren {
 private:
     std::string nume;
     std::string program;
     double pret;
-    std::vector<std::string> oreLibere; /// pentru generareOrelibere
+    std::vector<std::string> oreLibere;
+
+    std::string inceputProgram;
+    int inceputOra;
+    int inceputMinut;
+    std::string sfarsitProgram;
+    int sfarsitOra;
+    int sfarsitMinut;
 
 public:
-    /// Constructor
     Teren(const std::string& nume, const std::string& program, double pret)
             : nume(nume), program(program), pret(pret) {
         generareOreLibere();
     }
+
     ~Teren() {
         std::cout << "Destructorul pentru " << nume << " a fost apelat.\n";
     }
+
     void resetOreLibere() {
-        this->oreLibere.clear();
+        oreLibere.clear();
+        generareOreLibere();
     }
-    void stergeOra(std::string s) {
+
+    void stergeOra(const std::string& s) {
         auto it = std::find(oreLibere.begin(), oreLibere.end(), s);
-        this->oreLibere.erase(it);
+        if (it != oreLibere.end()) {
+            oreLibere.erase(it);
+        }
     }
-    ///oreLibere
+
     void generareOreLibere() {
-        resetOreLibere();
-        std::string inceputProgram = program.substr(0, 5); /// 10:00
-        int inceputOra = std::stoi(inceputProgram.substr(0, 2)); ///10
-        int inceputMinut = std::stoi(inceputProgram.substr(3, 2)); /// 00
+        oreLibere.clear();
+        std::string inceputProgram = getProgram().substr(0, 5); // 10:00
+        setInceputProgram(inceputProgram);
+        setInceputOra(std::stoi(getInceputProgram().substr(0, 2))); // 10
+        setInceputMinut(std::stoi(getInceputProgram().substr(3, 2))); // 00
 
-        std::string sfarsitProgram = program.substr(6, 5); ///12:00
-        int sfarsitOra = std::stoi(sfarsitProgram.substr(0, 2)); ///12
-        int sfarsitMinut = std::stoi(sfarsitProgram.substr(3, 2)); ///00
+        std::string sfarsitProgram = getProgram().substr(6, 5); // 12:00
+        setSfarsitProgram(sfarsitProgram);
+        setSfarsitOra(std::stoi(getSfarsitProgram().substr(0, 2))); // 12
+        setSfarsitMinut(std::stoi(getSfarsitProgram().substr(3, 2))); // 00
 
-        while (inceputOra < sfarsitOra || (inceputOra == sfarsitOra && inceputMinut <= sfarsitMinut)) {
-            std::string ora = (inceputOra < 10 ? "0" : "") + std::to_string(inceputOra) + ":" + (inceputMinut < 10 ? "0" : "") + std::to_string(inceputMinut);
+        while (getInceputOra() < getSfarsitOra() || (getInceputOra() == getSfarsitOra() && getInceputMinut() <= getSfarsitMinut())) {
+            std::string ora = (getInceputOra() < 10 ? "0" : "") + std::to_string(getInceputOra()) + ":" +
+                              (getInceputMinut() < 10 ? "0" : "") + std::to_string(getInceputMinut());
             oreLibere.push_back(ora);
-            inceputMinut += 30;
-            if (inceputMinut >= 60) {
-                inceputOra++;
-                inceputMinut -= 60;
+            setInceputMinut(getInceputMinut() + 30);
+            if (getInceputMinut() >= 60) {
+                setInceputOra(getInceputOra() + 1);
+                setInceputMinut(getInceputMinut() - 60);
             }
         }
-
     }
-    void afisareOreLibere() const {
-        std::cout << "Ore Libere pentru Terenul " << nume << ":" << std::endl;
+
+
+    /*void afisareOreLibere() const {
+        std::cout << "Ore Libere pentru Terenul " << nume << ":\n";
         for (const std::string& ora : oreLibere) {
             std::cout << ora << std::endl;
         }
-    }
+    }*/
+
     std::string getNume() const {
         return nume;
     }
+
     std::string getProgram() const {
         return program;
     }
+
     double getPret() const {
         return pret;
     }
+
     std::vector<std::string> getOreLibere() const {
         return oreLibere;
     }
@@ -72,17 +97,74 @@ public:
     void setNume(const std::string& nume) {
         this->nume = nume;
     }
+
     void setProgram(const std::string& program) {
         this->program = program;
-
         generareOreLibere();
     }
+
     void setPret(double pret) {
         this->pret = pret;
     }
+
+    // Getteri și setteri pentru variabilele adăugate
+
+    std::string getInceputProgram() const {
+        return inceputProgram;
+    }
+
+    void setInceputProgram(const std::string& inceputProgram) {
+        this->inceputProgram = inceputProgram;
+    }
+
+    int getInceputOra() const {
+        return inceputOra;
+    }
+
+    void setInceputOra(int inceputOra) {
+        this->inceputOra = inceputOra;
+    }
+
+    int getInceputMinut() const {
+        return inceputMinut;
+    }
+
+    void setInceputMinut(int inceputMinut) {
+        this->inceputMinut = inceputMinut;
+    }
+
+    std::string getSfarsitProgram() const {
+        return sfarsitProgram;
+    }
+
+    void setSfarsitProgram(const std::string& sfarsitProgram) {
+        this->sfarsitProgram = sfarsitProgram;
+    }
+
+    int getSfarsitOra() const {
+        return sfarsitOra;
+    }
+
+    void setSfarsitOra(int sfarsitOra) {
+        this->sfarsitOra = sfarsitOra;
+    }
+
+    int getSfarsitMinut() const {
+        return sfarsitMinut;
+    }
+
+    void setSfarsitMinut(int sfarsitMinut) {
+        this->sfarsitMinut = sfarsitMinut;
+    }
+
     friend std::ostream& operator<<(std::ostream& os, const Teren& teren) {
-        os << "Nume: " << teren.nume << ", Program: " << teren.oreLibere << ", Pret: " << teren.pret;
+        os << "Nume: " << teren.nume << ", Program: ";
+        for (const std::string& ora : teren.oreLibere) {
+            os << ora << " ";
+        }
+        os << ", Pret: " << teren.pret;
         return os;
-    };
+    }
 };
+
 #endif
